f982a44476ed314cb9da93159a302e3b608f2665
abdulrauf <abdulraufkhan824@gmail.com>
2023-07-21 19:32:21 +0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+// import { PrimeIconsModule } from 'primeicons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { WeatherService } from './weather.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, WeatherComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+
+    ToastrModule.forRoot({
+      timeOut: 3000, // Set the time (in milliseconds) for the toast to auto-dismiss
+      positionClass: 'toast-top-right', // Set the position of the toast notification
+      preventDuplicates: true, // Prevent duplicate toast messages
+    }),
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
